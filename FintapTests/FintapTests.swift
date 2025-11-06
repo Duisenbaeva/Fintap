396a9e422077635aa2bc98f4be206731d16933cb
Diana <diana.duisenbaeva@icloud.com>
2025-11-06 15:26:31 +0500

    Initial Commit

---
+++ FintapTests/FintapTests.swift
@@ -0,0 +1,17 @@
+//
+//  FintapTests.swift
+//  FintapTests
+//
+//  Created by diana on 06.11.2025.
+//
+
+import Testing
+@testable import Fintap
+
+struct FintapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
